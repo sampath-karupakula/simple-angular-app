4d7a7565cd190e074525336ff6f2792712d1ce2e
sampath-karupakula <karupakulasampath@gmail.com>
2016-12-03 09:06:27 +0530

    added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { AboutComponent } from './components/about/about.component';
 
-const appRoutes = [
+const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  {path: '/about', component: AboutComponent }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: HomeComponent },
 ]
 
 
@@ -24,7 +27,8 @@ const appRoutes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
